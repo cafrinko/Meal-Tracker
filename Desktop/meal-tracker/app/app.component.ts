fde430a8eb7d5bdc4e8686948f68473ad24ea430
Catherine Karra <catherine.karra@gmail.com>
2016-06-14 06:25:53 -0700

    instantiates a 2nd food to create an array of foods and displays each in template with a for loop attribute in html.

--- Desktop/meal-tracker/app/app.component.ts
+++ Desktop/meal-tracker/app/app.component.ts
@@ -5,20 +5,22 @@ import { Component } from 'angular2/core';
   template: `
   <div class="container">
     <h1>Meal Tracker</h1>
-    <h3>{{ food.name }}<br>{{ food.description }}<br>{{ food.calories }}</h3>
+    <h3 *ngFor="#food of foods">{{ food.name }}<br>{{ food.description }}<br>{{ food.calories }}</h3>
   </div>
   `
 })
 
 export class AppComponent {
-  public food: Food;
+  public foods: Food[];
   constructor(){
-    this.food = new Food("Ceviche", 0, "Octopus, cilantro, lemon, cucumbers", 400);
+    this.foods = [
+      new Food("Ceviche", 0, "Octopus, cilantro, lemon, cucumbers", 400),
+      new Food("Pie", 1, "Coconut cream, cacao, almonds, maple syrup", 550)
+    ];
   }
 }
 
 export class Food {
-  // public food: Food;
   constructor(public name: string, public id: number, public details: string, public calories: number) {
   }
 }
