4d98a65752927830a6542e35d47159e5fd0b8cd5
Catherine Karra <catherine.karra@gmail.com>
2016-06-14 06:14:59 -0700

    creates a food component with name, id, details, and calories properties; displays all properties except id in view; and creates an instance of a food object in the app component to serve as an example food log.

--- Desktop/meal-tracker/app/app.component.ts
+++ Desktop/meal-tracker/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from 'angular2/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'food-log',
   template: `
   <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
+    <h1>Meal Tracker</h1>
+    <h3>{{ food.name }}<br>{{ food.description }}<br>{{ food.calories }}</h3>
   </div>
   `
 })
 
 export class AppComponent {
+  public food: Food;
+  constructor(){
+    this.food = new Food("Ceviche", 0, "Octopus, cilantro, lemon, cucumbers", 400);
+  }
+}
 
+export class Food {
+  // public food: Food;
+  constructor(public name: string, public id: number, public details: string, public calories: number) {
+  }
 }
