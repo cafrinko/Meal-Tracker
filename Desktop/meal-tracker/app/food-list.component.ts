5fdd74719c774d3e5518358f2686e04c268ec4ab
Catherine Karra <catherine.karra@gmail.com>
2016-06-16 00:56:32 -0700

    adds a component to edit food details. when food is clicked on an edit food form shows

--- Desktop/meal-tracker/app/food-list.component.ts
+++ Desktop/meal-tracker/app/food-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { FoodComponent } from './food.component';
 import { Food } from './food.model';
+import { EditFoodDetailsComponent } from './edit-food-details.component';
 
 @Component({
   selector: 'food-list',
   inputs: ['foodList'],
   outputs: ['onFoodSelect'],
-  directives: [FoodComponent],
+  directives: [FoodComponent, EditFoodDetailsComponent],
   templateUrl: 'app/food-list.component.html'
 })
 export class FoodListComponent {
